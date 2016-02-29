ccafeca523c5c33158ba40ab478953c55fe6680b
verycarefully <vch@google.com>
2016-02-29 17:22:12 -0500

    Fail gracefully if task cannot be killed. Update copyright year.

--- planb/PBURLBuilder.h
+++ planb/PBURLBuilder.h
@@ -16,7 +16,7 @@
  is constructed for the 'sample' package, which is stored in the 'pkg1' folder of
  'mac.internal.megacorp.com', for a machine on the 'stable' configuration track.
 
- Copyright 2014 Google Inc.
+ Copyright 2016 Google Inc.
 
  Licensed under the Apache License, Version 2.0 (the "License"); you may not
  use this file except in compliance with the License.  You may obtain a copy
--- planb/main.m
+++ planb/main.m
@@ -5,7 +5,7 @@
 
  Installs management software on a managed Mac.
 
- Copyright 2014 Google Inc.
+ Copyright 2016 Google Inc.
 
  Licensed under the Apache License, Version 2.0 (the "License"); you may not
  use this file except in compliance with the License.  You may obtain a copy
