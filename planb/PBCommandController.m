ac5a3d798df875711374211f2f4170eb0e2a81ab
Russell Hancox <russell@hancox.us>
2017-06-12 17:05:42 -0400

    planb 2.0
    + Retry failed downloads up to 5 times
    + Refactor command handling
    + Make logging clearer
    + Create a power assertion for duration of run
    + Switch from pch to modules

---
+++ planb/PBCommandController.m
@@ -0,0 +1,104 @@
+/*
+  Copyright 2017 Google Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License.  You may obtain a copy
+  of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+  License for the specific language governing permissions and limitations under
+  the License.
+*/
+
+#import "PBCommandController.h"
+
+@interface PBCommandController ()
+@property NSTask *task;
+@end
+
+@implementation PBCommandController
+
+- (instancetype)init {
+  self = [super init];
+  if (self) {
+    _task = [[NSTask alloc] init];
+  }
+  return self;
+}
+
+- (NSString *)launchPath {
+  return self.task.launchPath;
+}
+
+- (void)setLaunchPath:(NSString *)launchPath {
+  self.task.launchPath = [launchPath copy];
+}
+
+- (NSArray *)arguments {
+  return self.task.arguments;
+}
+
+- (void)setArguments:(NSArray *)arguments {
+  self.task.arguments = [arguments copy];
+}
+
+- (NSDictionary *)environment {
+  return self.task.environment;
+}
+
+- (void)setEnvironment:(NSDictionary *)environment {
+  self.task.environment = [environment copy];
+}
+
+- (int)launchWithOutput:(NSString *__autoreleasing *)output {
+  self.task.standardInput = [NSPipe pipe];
+  self.task.standardOutput = self.task.standardError = [NSPipe pipe];
+
+  __weak typeof(self) weakSelf = self;
+  __block NSException *exception;
+
+  dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+    typeof(weakSelf) self = weakSelf;
+
+    @try {
+      [self.task launch];
+
+      if (self.standardInput) {
+        NSData *data = [self.standardInput dataUsingEncoding:NSUTF8StringEncoding];
+        NSFileHandle *inputFh = [self.task.standardInput fileHandleForWriting];
+
+        [inputFh writeData:data];
+        [inputFh closeFile];
+      }
+
+      [self.task waitUntilExit];
+    } @catch (NSException *e) {
+      exception = e;
+    } @finally {
+      dispatch_semaphore_signal(sema);
+    }
+  });
+  dispatch_semaphore_wait(sema, dispatch_time(DISPATCH_TIME_NOW, self.timeout * NSEC_PER_SEC));
+
+  if ([self.task isRunning]) {
+    kill(self.task.processIdentifier, SIGKILL);
+  }
+
+  if (exception) {
+    if (output) *output = exception.reason;
+    return -1;
+  }
+
+  if (output) {
+    NSData *availableData = [[self.task.standardOutput fileHandleForReading] availableData];
+    *output = [[NSString alloc] initWithData:availableData encoding:NSUTF8StringEncoding];
+  }
+  return self.task.terminationStatus;
+}
+
+@end
--- planb/PBPackageInstaller.h
+++ planb/PBPackageInstaller.h
@@ -1,37 +1,45 @@
 /*
+  Copyright 2016 Google Inc.
 
- Plan B
- PBPackageInstaller.h
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License.  You may obtain a copy
+  of the License at
 
- Copyright 2016 Google Inc.
+  http://www.apache.org/licenses/LICENSE-2.0
 
- Licensed under the Apache License, Version 2.0 (the "License"); you may not
- use this file except in compliance with the License.  You may obtain a copy
- of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- License for the specific language governing permissions and limitations under
- the License.
-
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+  License for the specific language governing permissions and limitations under
+  the License.
 */
 
-/// Mount dmg disk image to install first pkg in payload, then unmount and remove dmg.
+@import Foundation;
+
+/// TODO: fill!
 @interface PBPackageInstaller : NSObject
 
+- (instancetype)init NS_UNAVAILABLE;
+
 /// Designated initializer.
-/// @param receiptName pkgutil receipt name to forget before installation, like 'com.megacorp.pkg'.
-/// @param packagePath path to package to mount , like '/tmp/planb-dmg.ihI1UV/pkg-stable.dmg'
-/// @param targetVolume target disk volume to install package to.
-- (instancetype)initWithReceiptName:(NSString *)receiptName
-                        packagePath:(NSString *)packagePath
-                       targetVolume:(NSString *)targetVolume;
+- (instancetype)initWithURL:(NSURL *)packageURL
+                receiptName:(NSString *)receipt NS_DESIGNATED_INITIALIZER;
+
+/// The NSURLSession to use for downloading packages. If not set, a default one will be used.
+@property NSURLSession *session;
+
+/// The number of seconds to allow downloading before timing out. Defaults to 300 (5 minutes).
+@property NSUInteger downloadTimeoutSeconds;
+
+/// The number of download attempts before giving up. Defaults to 5.
+@property NSUInteger downloadAttemptsMax;
+
+/// A prefix to prepend to all lines logged by this class.
+@property(copy) NSString *logPrefix;
 
 /// Mount disk image and install package.
-- (void)installApplication;
+/// @return YES if installation was successful
+- (BOOL)install;
 
 @end
 
